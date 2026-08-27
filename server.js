34cf2d6a3f39118e0368cd1f6133463ef2643092
Diyorbek <bakhtiyorovdiyorbek314@gmail.com>
2026-08-27 20:18:02 +0900

    feat: Finished Portfolio main page

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log("Web serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 //1 Kirish code
 app.use(express.static("public"));
@@ -25,13 +35,17 @@ app.set("view engine", "ejs");
 // app.get("/biznes", function (req, res) {
 //   res.render("biznes");
 // });
-app.post("/create-item", (req, res) => {
-  console.log(req.body);
-  res.json({ test: "success" });
-});
+// app.post("/create-item", (req, res) => {
+//   console.log(req.body);
+//   res.json({ test: "success" });
+// });
 
-app.get("/", function (req, res) {
-  res.render("harid");
+// app.get("/", function (req, res) {
+//   res.render("harid");
+// });
+
+app.get("/develop", function (req, res) {
+  res.render("develop", { user: user });
 });
 
 const server = http.createServer(app);
