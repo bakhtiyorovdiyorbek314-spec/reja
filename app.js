a37bae4a9092994257bc48765af4e76b9849fda6
Diyorbek <bakhtiyorovdiyorbek314@gmail.com>
2026-08-31 00:53:08 +0900

    feat:mongodb install

---
+++ app.js
@@ -0,0 +1,40 @@
+console.log("Web serverni boshlash");
+const express = require("express");
+const app = express();
+const res = require("express/lib/response");
+
+//MongoDB
+const db = require("./server");
+
+//1 Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//2 Session
+
+//3 Views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+//4 Routing code
+
+// app.get("/hello", function (req, res) {
+//   res.end(`<h1 style="color:brown" > HELLO WORLD by DEEN</h1>`);
+// });
+
+// app.get("/gift", function (req, res) {
+//   res.end(`<h1 style="color:blue" > You are on gift page</h1>`);
+// });
+// app.get("/biznes", function (req, res) {
+//   res.render("biznes");
+// });
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  res.json({ test: "success" });
+});
+
+app.get("/", function (req, res) {
+  res.render("reja");
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,43 +1,30 @@
-console.log("Web serverni boshlash");
-const express = require("express");
-const app = express();
 const http = require("http");
+const app = require("./app");
+const mongodb = require("mongodb");
 
-//1 Kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-//2 Session
-
-//3 Views code
-app.set("views", "views");
-app.set("view engine", "ejs");
-//4 Routing code
-
-// app.get("/hello", function (req, res) {
-//   res.end(`<h1 style="color:brown" > HELLO WORLD by DEEN</h1>`);
-// });
-
-// app.get("/gift", function (req, res) {
-//   res.end(`<h1 style="color:blue" > You are on gift page</h1>`);
-// });
-// app.get("/biznes", function (req, res) {
-//   res.render("biznes");
-// });
-app.post("/create-item", (req, res) => {
-  console.log(req.body);
-  res.json({ test: "success" });
-});
-
-app.get("/", function (req, res) {
-  res.render("reja");
-});
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-  console.log(
-    `The server is running successfully on port:${PORT} , http://localhost:${PORT}`,
-  );
-});
+let db;
+const connectionString =
+  "mongodb+srv://Deen:7H96VgAGWckOf9HJ@cluster0.dhe4gj5.mongodb.net/Reja";
+mongodb.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("ERROR on connection MongoDB");
+    else {
+      console.log("MongoDB connection succeed");
+      //   console.log(client);
+      module.exports = client;
+      const mongodb = require("mongodb");
+      const server = http.createServer(app);
+      let PORT = 3000;
+      server.listen(PORT, function () {
+        console.log(
+          `The server is running successfully on port:${PORT} , http://localhost:${PORT}`,
+        );
+      });
+    }
+  },
+);
